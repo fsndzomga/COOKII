6a5f442305532687d49fc8bd3c2a002ccbdf3d3f
Alizee Vallat <alizee.v@live.fr>
2023-02-28 10:20:53 +0100

    update controller Bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,32 @@
 class BookingsController < ApplicationController
-  # def new
-  # end
+  def new
+    @meal = Meal.find(params[:meal_id]) # pourquoi ca ne fonctionne pas ?
+    @booking = Booking.new
+  end
 
-  # def index
-  # end
+  def index
+    @bookings = Booking.all
+  end
 
-  # def create
-  # end
+  def create
+    @meal = Meal.find(params[:meal_id])
+    @user_id = User.find(params[:current_user_id]) # vérifier ???
+    @booking = Booking.new(booking_params)
+    @booking.meal = @meal
+    @booking.user_id = @user_id
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   # def updated
   # end
 
-  # private
-  
+  private
+
+  def booking_params
+    params.require(:bookings).permit(:date, :number_of_persons)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :meals
+  resources :meals do
+    resources :bookings, only: %i[new create]
+  end
+  resources :bookings, only: %i[index update]
 end
