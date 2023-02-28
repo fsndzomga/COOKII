c71ecd0023c736328db2262fd66ba74952cc4ebb
Milagros Lasarte <lasartemilagros@gmail.com>
2023-02-28 12:06:54 +0100

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Meal.destroy_all
+User.destroy_all
+
+puts 'Creating user and meals...'
+15.times do
+  user = User.new(
+    email: Faker::Internet.free_email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    password: "topsecret",
+    password_confirmation: "topsecret"
+  )
+  user.save!
+  meal = Meal.new(
+    name: Faker::Food.dish,
+    description: Faker::Food.description,
+    vegetarian: [true, false].sample,
+    category: Faker::Food.ethnic_category,
+    price_per_person: rand(5..15),
+    max_person: rand(1..8),
+    user: user
+  )
+  meal.save!
+end
+puts 'Finished!'
