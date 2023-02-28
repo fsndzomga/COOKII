8d8a61b100dff4b3e375b1bcbb1c9895aeb9694d
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-02-28 17:28:17 +0100

    added pundit to booking destroy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -34,6 +34,7 @@ class BookingsController < ApplicationController
 
   def destroy
     @booking = Booking.find(params[:id])
+    authorize @booking
     @booking.destroy
     redirect_to bookings_path
   end
