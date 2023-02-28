5ef12b55260d166580560eb0b94759fabd580dce
Alizee Vallat <alizee.v@live.fr>
2023-02-28 11:12:14 +0100

    last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-  get 'bookings/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
