cf9ba8ef179ae9f0867340ddafcb9f66b2966744
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-03-01 10:56:06 +0100

    m

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = policy_scope(Booking)
+    @bookings = Booking.all
   end
 
   def show
--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,11 +1,9 @@
 class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    def resolve
-      scope.where(user: user)
-    #   # scope.joins(bookings: :meals).where("bookings.user = user and meals.user = user")
-    #   # scope.joins(bookings: [meals: :meal_id]).where(meal_id: { user: user })
-    end
+    # def resolve
+    #   scope.where(user: user)
+    # end
   end
 
   def show?
