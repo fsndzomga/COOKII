c36880b446f47af9ec59cc837bebd592389b63dc
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-03-01 08:26:35 +0100

    pundit

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -2,15 +2,15 @@ class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      # scope.where(user: user)
-      # scope.joins(bookings: :meals).where("bookings.user = user and meals.user = user")
-      # scope.joins(bookings: [meals: :meal_id]).where(meal_id: { user: user })
+      scope.where(user: user)
+    #   # scope.joins(bookings: :meals).where("bookings.user = user and meals.user = user")
+    #   # scope.joins(bookings: [meals: :meal_id]).where(meal_id: { user: user })
     end
   end
 
-  # def show?
-  #   true
-  # end
+  def show?
+    true
+  end
 
   def new?
     create?
@@ -29,5 +29,6 @@ class BookingPolicy < ApplicationPolicy
   end
 
   def destroy?
+    user == record.user
   end
 end
