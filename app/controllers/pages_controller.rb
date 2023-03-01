75f844181270b85b456e6d984e434e4963809fb0
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-03-01 20:53:25 +0100

    dashboard route, pages controller, view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def dashboard
+    @meals = Meal.where(user: current_user)
+    # @bookings = @meals.map { |meal| meal.bookings }
+  end
 end
