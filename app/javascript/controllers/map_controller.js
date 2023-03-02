49b5f5c2397eb4fc65367a6cb32c637f03c5555e
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-03-02 15:02:24 +0100

    geocoding with markers and centering

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -17,6 +17,7 @@ export default class extends Controller {
     })
 
     this.#addMarkersToMap()
+    this.#fitMapToMarkers()
   }
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
@@ -24,5 +25,11 @@ export default class extends Controller {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
     })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
