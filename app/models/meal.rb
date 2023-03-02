e4eb159a1ea16dda8b010433d4246559bfb3260d
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-03-02 15:29:50 +0100

    fixed list of meal categories

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,12 +1,14 @@
 class Meal < ApplicationRecord
+  CATEGORIES = ["Asian", "European", "Mediterranean", "African"]
   belongs_to :user
   has_one_attached :photo
   before_save :attach_photo
   validates :name, :price_per_person, :max_person, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 
   # Search Bar
   include PgSearch::Model
-  pg_search_scope :search_in_meal, 
+  pg_search_scope :search_in_meal,
   against: [:name, :description, :category],
   using: {
     tsearch: { prefix: true }
