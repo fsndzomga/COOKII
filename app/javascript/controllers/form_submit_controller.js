5b8695c2bfcefa5d6132c6917ee4714454809c36
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-03-04 12:25:28 +0100

    form submit controller

--- app/javascript/controllers/form_submit_controller.js
+++ app/javascript/controllers/form_submit_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="form-submit"
 export default class extends Controller {
-  connect() {
+  submit() {
+    this.element.submit();
   }
 }
