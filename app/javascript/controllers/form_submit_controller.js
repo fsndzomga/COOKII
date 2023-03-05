6c963ad808c4d750d6b120c543247ea008ba12be
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-03-05 12:35:54 +0100

    reviews updated

--- app/javascript/controllers/form_submit_controller.js
+++ app/javascript/controllers/form_submit_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
+  static targets = ["form"];
+
+  connect() {
+
+  }
+
   submit() {
-    console.log("here");
+    this.formTarget.submit();
   }
 }
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
   validates :rating, presence: true, inclusion: { in: 1..5 }
-  validates :content, :rating, presence: true, length: { minimum: 10 }
+  validates :content, :rating, presence: true
+  # , length: { minimum: 10 }
 end
