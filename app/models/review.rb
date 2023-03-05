eca922603fb4ca6c44e5572fb04aa05b81a790d4
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-03-05 12:43:51 +0100

    code optimization

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,4 @@ class Review < ApplicationRecord
   belongs_to :booking
   validates :rating, presence: true, inclusion: { in: 1..5 }
   validates :content, :rating, presence: true
-  # , length: { minimum: 10 }
 end
